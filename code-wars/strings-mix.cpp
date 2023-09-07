c100e281c602bba5f32b597b7744f631df55b28d
Lucas <lucasnpc2@outlook.com>
2023-09-07 20:49:14 -0300

    Strings mix code wars

--- code-wars/strings-mix.cpp
+++ code-wars/strings-mix.cpp
@@ -1,9 +1,9 @@
-#include <string>
 #include <iostream>
-#include <map>
-#include <cstring>
 #include <algorithm>
+#include <unordered_map>
+#include <vector>
 #include <set>
+#include <numeric>
 
 class Mix
 {
@@ -11,76 +11,53 @@ public:
     static std::string mix(const std::string& s1, const std::string& s2);
 };
 
-std::string Mix::mix(const std::string& s1, const std::string& s2) {
-    std::map<char, int> map, map2;
-    std::set<char> set1, set2;
-    std::string result;
+std::unordered_map<char, int> counter(std::string s) {
+    std::unordered_map<char, int> x;
+    std::sort(s.begin(), s.end());
+    for (auto c : s) {
+        if (isalpha(c)) {
+            x[c]++;
+        }
+    }
+    return x;
+}
 
+std::string Mix::mix(const std::string& s1, const std::string& s2) {
+    std::unordered_map<char, int> set1 = counter(s1);
+    std::unordered_map<char, int> set2 = counter(s2);
+    std::vector<std::string> res;
+    std::set<char> keys;
     for (auto& c : s1) {
         if (isalpha(c) && islower(c)) {
-            set1.insert(c);
-            map[c]++;
+            keys.insert(c);
         }
     }
     for (auto& c : s2) {
         if (isalpha(c) && islower(c)) {
-            set2.insert(c);
-            map2[c]++;
+            keys.insert(c);
         }
     }
-
-    std::set<char, std::function<bool(char, char)>> mixedSet([&map, &map2](char a, char b) {
-        if (map[a] == map2[a] || map[a] == map[b] || map2[a] == map2[b]) {
-            return a < b;
+    for (auto& key : keys) {
+        auto c1 = set1[key];
+        auto c2 = set2[key];
+        auto count = std::max(c1, c2);
+        if (count > 1) {
+            auto from = count > c1 ? '2' : count > c2 ? '1' : '=';
+            auto str = std::string(count, key);
+            res.push_back(std::string(1, from) + ":" + str);
         }
-        else if (map[a] > map[b] || map2[a] > map2[b]) {
-            return map[a] < map[b] || map2[a] < map2[b];
-        }
-        return a > b;
+    }
+    std::sort(res.begin(), res.end(), [](const auto& a, const auto& b) {
+        return a.size() == b.size() ? a < b : a.size() > b.size();
         });
-
-    for (auto& c : set1) {
-        if (map[c] > 1) {
-            mixedSet.insert(c);
-        }
-    }
-
-    for (auto& c : set2) {
-        if (map2[c] > 1) {
-            mixedSet.insert(c);
-        }
-    }
-
-    for (auto c : mixedSet) {
-        if (map[c] > map2[c]) {
-            result += "1:";
-            for (int i = 0; i < map[c]; ++i) {
-                result += c;
-            }
-            result += "/";
-        }
-        else if (map2[c] > map[c]) {
-            result += "2:";
-            for (int i = 0; i < map2[c]; ++i) {
-                result += c;
-            }
-            result += "/";
-        }
-        else {
-            result += "=:";
-            for (int i = 0; i < map2[c]; ++i) {
-                result += c;
-            }
-            result += "/";
-        }
-    }
-
-    return result.substr(0, result.size() - 1);
+    return std::accumulate(res.begin(), res.end(), std::string(""), [](const auto& a, const auto& b) {
+        return a.empty() ? b : a + "/" + b;
+        });
 }
 
 int main() {
     Mix mix;
-    std::cout << mix.mix("eeee bbb rr hh dd yyyyy jj ccc", "eeee bbb rr hh dd yyyyy jj ccc uuu") << "\n";
+    std::cout << mix.mix("eeee DDE bbb rr hh dd yyyyy jj ccc", "eeee bbb rr hh dd yyyyy jj ccc uuu") << "\n";
     std::cout << mix.mix("ooo uuu", "sss") << "\n";
     std::cout << mix.mix("rr hh", "eeeee yy rr hh") << "\n";
     std::cout << mix.mix("ooo uuu nnn ii gg", "sss nnn aa dd ee gg") << "\n";
