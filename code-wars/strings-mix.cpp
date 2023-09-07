aa791f80307b87ee250a3b42924e8770b71bb26b
Lucas <lucasnpc2@outlook.com>
2023-09-07 20:29:27 -0300

    wip: testing

--- code-wars/strings-mix.cpp
+++ code-wars/strings-mix.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <cstring>
 #include <algorithm>
+#include <set>
 
 class Mix
 {
@@ -12,53 +13,45 @@ public:
 
 std::string Mix::mix(const std::string& s1, const std::string& s2) {
     std::map<char, int> map, map2;
-    std::string result, result2, mixedStr;
+    std::set<char> set1, set2;
+    std::string result;
 
     for (auto& c : s1) {
         if (isalpha(c) && islower(c)) {
-            result += c;
+            set1.insert(c);
             map[c]++;
         }
     }
     for (auto& c : s2) {
         if (isalpha(c) && islower(c)) {
-            result2 += c;
+            set2.insert(c);
             map2[c]++;
         }
     }
 
-    mixedStr = result + result2;
+    std::set<char, std::function<bool(char, char)>> mixedSet([&map, &map2](char a, char b) {
+        if (map[a] == map2[a] || map[a] == map[b] || map2[a] == map2[b]) {
+            return a < b;
+        }
+        else if (map[a] > map[b] || map2[a] > map2[b]) {
+            return map[a] < map[b] || map2[a] < map2[b];
+        }
+        return a > b;
+        });
 
-    std::string distictResult;
-
-    for (char c : mixedStr) {
-        if (distictResult.find(c) == std::string::npos && (map[c] > 1 || map2[c] > 1))
-        {
-            distictResult += c;
+    for (auto& c : set1) {
+        if (map[c] > 1) {
+            mixedSet.insert(c);
         }
     }
 
-    std::sort(distictResult.begin(), distictResult.end(), [&map, &map2](char a, char b) {
-        if (map[a] == map[b])
-        {
-            return map[a] < map[b];
+    for (auto& c : set2) {
+        if (map2[c] > 1) {
+            mixedSet.insert(c);
         }
-        return map[a] > map[b];
-        });
+    }
 
-    std::sort(distictResult.begin(), distictResult.end(), [&map, &map2](char a, char b) {
-        if (map2[a] == map2[b])
-        {
-            return map2[a] < map2[b];
-        }
-        return map2[a] > map2[b];
-        });
-
-    std::cout << "Distinct: " << distictResult << "\n";
-
-    result = "";
-
-    for (auto c : distictResult) {
+    for (auto c : mixedSet) {
         if (map[c] > map2[c]) {
             result += "1:";
             for (int i = 0; i < map[c]; ++i) {
@@ -87,6 +80,9 @@ std::string Mix::mix(const std::string& s1, const std::string& s2) {
 
 int main() {
     Mix mix;
-    std::cout << mix.mix("eeee bbb dd yyyyy jj ccc", "eeee bbb dd yyyyy jj ccc uuu") << "\n";
+    std::cout << mix.mix("eeee bbb rr hh dd yyyyy jj ccc", "eeee bbb rr hh dd yyyyy jj ccc uuu") << "\n";
+    std::cout << mix.mix("ooo uuu", "sss") << "\n";
+    std::cout << mix.mix("rr hh", "eeeee yy rr hh") << "\n";
+    std::cout << mix.mix("ooo uuu nnn ii gg", "sss nnn aa dd ee gg") << "\n";
     return 0;
 }
